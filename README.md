cca3f5b7bc8a119ea518082664778c8b4ee5abb1
Oleh Sklyarenko <osklyarenko@morningcoffeesoft.com>
2014-10-30 23:07:41 +0200

    Added a readme file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-when
+README
 ====
+To launch execute
+	rspec  environment_spec.rb --color
+
+Environment
+* Mac OS X 10.9
+* Ruby 1.9.3
--- environment_spec.rb
+++ environment_spec.rb
@@ -1,8 +1,12 @@
 require './definitions.rb'
 
 describe 'Java'.def do
-	it 'is configured' do		
-		'Java'.def.should have_JAVA_HOME_set
+	it 'is installed' do		
 		'Java'.def.should be_installed		
 	end
+
+	it 'has JAVA_HOME set' do		
+		'Java'.def.should have_JAVA_HOME_set		
+	end
+
 end
