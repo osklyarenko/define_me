e69436b464dc633a6cf11e2e57047f6c8aba8f37
Oleh Sklyarenko <osklyarenko@morningcoffeesoft.com>
2014-10-30 21:22:31 +0200

    Integrating with RSpec

--- environment_spec.rb
+++ environment_spec.rb
@@ -1,3 +1,7 @@
-describe 'Environment' do
-	it "Java is installed" {false}
+require './definitions.rb'
+
+describe 'Java'.def do
+	it 'is installed' do		
+		'Java'.def.should be_is_installed
+	end
 end
